0c534904f089e39a2e372d85247c0390b171f236
gianbdev <giancarlo.silva550@gmail.com>
2024-09-23 18:11:51 -0500

    update production

---
+++ app/Models/Generals/Pedido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Generals;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    use HasFactory;
+}
